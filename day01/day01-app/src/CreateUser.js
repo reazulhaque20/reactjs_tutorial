e8000f3d624c0f43a26a1442ad32b4981179df39
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2024-03-18 00:08:42 +0800

    zxc

--- day01/day01-app/src/CreateUser.js
+++ day01/day01-app/src/CreateUser.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 
 function CreateUser() {
+  const [userId, setUserId] = useState("");
+  const [welcomeMsg, setWelcomeMsg] = useState("");
+  const createUser = (event) => {
+    setWelcomeMsg(`Welcome ${userId}`);
+    event.preventDefault();
+  };
   return (
     <Container>
       <Row className="vh-100 d-flex justify-content-center align-items-center">
@@ -9,12 +16,20 @@ function CreateUser() {
           <Card className="shadow">
             <Card.Body>
               <div className="mb-3 mt-4">
-                <h2 className="fw-bold mb-2 text-uppercase text-center">EDMS</h2>
-                <p className=" mb-5 text-center">Please enter your login and password!</p>
-                <Form className="mb-3">
+                <h2 className="fw-bold mb-2 text-uppercase text-center">
+                  EDMS
+                </h2>
+                <p className=" mb-5 text-center">
+                  Please enter your login and password!
+                </p>
+                <Form className="mb-3" onSubmit={createUser}>
                   <Form.Group className="mb-3" controlId="userId">
                     <Form.Label>User ID</Form.Label>
-                    <Form.Control type="text" placeholder="Enter user ID" />
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter user ID"
+                      onBlur={(event) => setUserId(event.target.value)}
+                    />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="password">
@@ -33,8 +48,9 @@ function CreateUser() {
                       Login
                     </Button>
                   </div>
-                </Form>
 
+                  <Form.Label>{welcomeMsg}</Form.Label>
+                </Form>
               </div>
             </Card.Body>
           </Card>
